1f29c73ac84d43e40fb68ab4df6865a7231e4981
thibastos0 <thibastos@live.com>
2026-03-29 13:51:48 -0300

    Adicionada animação heart_beat ao logotipo e implementação na MainActivity.

--- app/src/main/java/com/example/sosengasgo_android/MainActivity.java
+++ app/src/main/java/com/example/sosengasgo_android/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.sosengasgo_android;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +15,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        */
+
+
+
     }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        /*
+        ImageView imgLogo = findViewById(R.id.img_logo);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.heart_beat);
+        animation.setRepeatCount(1);
+        imgLogo.startAnimation(animation);*/
+
+        if (hasFocus) {
+            final ImageView imgLogo = findViewById(R.id.img_logo);
+            final Animation anim = AnimationUtils.loadAnimation(this, R.anim.heart_beat);
+
+            imgLogo.startAnimation(anim);
+
+            new android.os.Handler().postDelayed(() -> imgLogo.startAnimation(anim), 650);
+        }
+    }
+
 }
